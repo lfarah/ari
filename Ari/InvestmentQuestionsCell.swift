628295ef5c9307102d97ba1ced9d7c5217440122
Lucas Farah <lucasfarah@Lucass-MacBook-Pro.local>
2015-03-07 05:01:11 -0800

    Screens 2,3,6 created

---
+++ Ari/InvestmentQuestionsCell.swift
@@ -0,0 +1,27 @@
+//
+//  InvestmentQuestionsCell.swift
+//  Ari
+//
+//  Created by Lucas Farah on 3/7/15.
+//  Copyright (c) 2015 Lucas Farah. All rights reserved.
+//
+
+import UIKit
+
+class InvestmentQuestionsCell: UITableViewCell {
+
+    @IBOutlet weak var imgvInput: UIImageView!
+    @IBOutlet weak var txtInput: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
